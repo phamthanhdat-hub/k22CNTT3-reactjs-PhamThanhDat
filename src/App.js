bb75313fe1ba70342038ced1bf2cd88cec28a688
phamthanhdat-hub <thanhdat160924@gmail.com>
2024-03-29 08:55:23 +0700

    phamthanhdat

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './1.jpg';
 import './App.css';
 
 function App() {
@@ -7,16 +7,12 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        DAI HOC NGUYEN TRAI
+      </p>
+        <div>
+        <h2>K22-CNTT3-Pham Thanh Dat</h2>
+        </div>
+      
       </header>
     </div>
   );
